89d0856da9468425a966c07dc91474f97ada13c5
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-03-05 05:48:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction(name, languaje)
-  puts "Hi, my name is #{name} and I am learning to program in #{languaje}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
